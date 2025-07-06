3dc044c09885e0f412e3ad423161d9d9527d3885
rucha <ruchachavan21@gmail.com>
2025-07-06 15:20:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-400 to-pink-500 text-white text-4xl font-bold">
+      Tailwind is Working 🎉
+    </div>
+  );
+}
+
+export default App;
